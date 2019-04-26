e0facabf3803f25df0fbd40342f095a88f839348
web223dev <ruslanus866@outlook.com>
2019-04-26 16:57:32 +0800

    Github corner finished

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import GithubCorner from 'components/github/Corner';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <GithubCorner />
+      </div>
+    );
+  }
+}
+
+export default App;
